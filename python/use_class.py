beee15f964f9159105345bf2cb27c807e0ad97d3
spacetrav <55599917+spacetrav@users.noreply.github.com>
2020-01-14 13:29:50 -0600

    Add files via upload

---
+++ python/use_class.py
@@ -0,0 +1,18 @@
+# inclusion
+from person import Person
+
+p = Person('Travis', 22)
+p.say_name()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
